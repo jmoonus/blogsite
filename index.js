b674789bf8f8f176d26828f66e318845ef384f68
Jasmine <jasminekang@cmail.carleton.ca>
2021-01-06 16:54:03 -0500

    Blog CRUD requests

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //Imports
 const express = require("express");
 const bodyParser = require("body-parser");
+const database = require("./config/database")
+const Blog = require("./models/blog.model")
 
 const app = express();
 const port = 5000; 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 //GET request 
 app.get('/', (req, res) => {
@@ -17,6 +20,44 @@ app.get('/', (req, res) => {
     })
 })
 
+//Blog CRUD request 
+app.post("/api/blog", async (req, res) => {
+
+    const blog = await new Blog({
+        ...req.body
+    })
+    blog.save()
+
+    res.json({
+        message: "Created blog! :)"
+    })
+})
+
+app.get("/api/blog", async (req, res) => {
+    const blogs = await Blog.find()
+
+    res.json({
+        blogs
+    })
+})
+
+app.put("/api/blog", async (req, res) => {
+    await Blog.updateOne({_id: req.body.id}, req.body)
+
+    res.json({
+        message: "Edited blog " + req.body.id
+    })
+})
+
+app.delete("/api/blog", async (req, res) => {
+
+    await Blog.findByIdAndDelete(req.body.id)
+
+    res.json({
+        message: "Deleted blog " + req.body.id
+    })
+})
+
 app.listen(port, () => {
-    console.log("The server is not running on the port", port);
+    console.log("The server is running on the port", port);
 })
